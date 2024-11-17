08e74d0a61ec55fbc607ba161749a79ce3c1ef51
PrajwalSinghBhadouria <151111886+PrajwalSinghBhadouria@users.noreply.github.com>
2024-11-17 08:00:08 +0530

    Update Climbing_stairs.cpp

--- Climbing_stairs.cpp
+++ Climbing_stairs.cpp
@@ -6,7 +6,6 @@ public:
         vector<int> dp(n+1,-1);
         dp[0]=1;
         dp[1]=1;
-        dp.push_back(1);
         for(int i=2; i<=n; i++){
             dp[i]= dp[i-1]+dp[i-2];
         }
